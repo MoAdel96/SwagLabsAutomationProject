3357cf3c585a388683d8c832b919ed139a9c7787
Your Name <your.email@example.com>
2024-09-08 16:51:09 +0300

    Initial commit

---
+++ src/main/java/Utilities/Utility.java
@@ -0,0 +1,77 @@
+package Utilities;
+
+import io.qameta.allure.Allure;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+public class Utility{
+
+    private static final String SCREENSHOTS_PATH = "test-outputs/Screenshots/";
+
+    public static void clickingOnElement(WebDriver driver, By locator) {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(locator));
+        driver.findElement(locator).click();
+    }
+
+    public static void sendData(WebDriver driver, By locator, String data) {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+        driver.findElement(locator).sendKeys(data);
+    }
+
+    public static String getText(WebDriver driver, By locator) {
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(locator));
+        return driver.findElement(locator).getText();
+
+    }
+    public static WebDriverWait generalWait(WebDriver driver) {
+        return new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    public static void scrolling(WebDriver driver, By locator) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", findWebElement(driver, locator));
+    }
+
+    public static WebElement findWebElement(WebDriver driver, By locator) {
+        return driver.findElement(locator);
+    }
+
+
+    public static String getTimeStamp() {
+        return new SimpleDateFormat("yyyy-MM-dd-hh-mm-ssa").format(new Date());
+    }
+
+
+    public static void takeScreenShot(WebDriver driver, String screenshotName) {
+        try {
+            // Capture screenshot using TakesScreenshot
+            File screenshotSrc = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+            // Save screenshot to a file if needed
+            File screenshotFile = new File(SCREENSHOTS_PATH + screenshotName + "-" + getTimeStamp() + ".png");
+            FileUtils.copyFile(screenshotSrc, screenshotFile);
+
+            // Attach the screenshot to Allure
+            Allure.addAttachment(screenshotName, Files.newInputStream(Path.of(screenshotFile.getPath())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectingFromDropDown(WebDriver driver, By locator, String option) {
+        new Select(findWebElement(driver, locator)).selectByVisibleText(option);
+    }
+}
+
