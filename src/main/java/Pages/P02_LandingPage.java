9db99af29ea2838314d1bc037c14c5c8c0d7df44
Adel <muhamed.adel96@gmail.com>
2024-09-15 20:11:12 +0300

    tc 2 part 1

--- src/main/java/Pages/P02_LandingPage.java
+++ src/main/java/Pages/P02_LandingPage.java
@@ -1,8 +1,69 @@
 package Pages;
 
+import Utilities.LogsUtils;
+import Utilities.Utility;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class P02_LandingPage {
+    private final By addToCartButtonForAllProducts = By.xpath("//button[@class]");
+    private final By numberOfProductsOnCartIcon = By.className("shopping_cart_badge");
+    private final WebDriver driver;
+    private static List<WebElement> allProducts;
+    private static List<WebElement> SelectedProducts;
+    private final By numberOfSelectedProducts = By.xpath("//button[text()='REMOVE']");
+
     public P02_LandingPage(WebDriver driver) {
+        this.driver = driver;
     }
+
+    public P02_LandingPage addAllProductsToCart() {
+        allProducts = driver.findElements(addToCartButtonForAllProducts);
+       LogsUtils.info("number of all products" + allProducts.size());
+        for (int i = 1; i <= allProducts.size(); i++) {
+            By addToCartButtonForAllProducts = By.xpath("//button[@class][" + i + "]");
+            Utility.clickingOnElement(driver, addToCartButtonForAllProducts);
+
+        }
+        return this;
+    }
+
+    public String getNumberOfProductsOnCartIcon() {
+
+        try {
+
+            LogsUtils.info("number of products on cart " + Utility.getText(driver, numberOfProductsOnCartIcon));
+            return Utility.getText(driver, numberOfProductsOnCartIcon);
+        } catch (Exception e) {
+            LogsUtils.error(e.getMessage());
+        }
+
+        return "0";
+    }
+
+
+
+    public String getNumberOfSelectedProducts(){
+
+        try{
+        SelectedProducts=driver.findElements(numberOfSelectedProducts);
+        LogsUtils.info("selected products " + SelectedProducts.size());
+
+        return String.valueOf(SelectedProducts.size());}
+        catch (Exception e){
+            LogsUtils.error(e.getMessage());
+            return "0";
+        }
+    }
+
+
+
+    public boolean comparingNumberOfSelectedProductsWithCart() {
+        return getNumberOfProductsOnCartIcon().equals(getNumberOfSelectedProducts());
+    }
+
+
 }
--- src/test/java/Tests/TC01_LoginTest.java
+++ src/test/java/Tests/TC01_LoginTest.java
@@ -2,17 +2,21 @@ package Tests;
 
 import Pages.P01_LoginPage;
 import Utilities.DataUtils;
+import Utilities.LogsUtils;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
+import Listeners.IInvokedMethodListenerClass;
+import Listeners.ITestResultListenerClass;
 import java.io.IOException;
 import java.time.Duration;
 
-import static DriverFactory.DriverFactory.getDriver;
-import static DriverFactory.DriverFactory.setupDriver;
+import static DriverFactory.DriverFactory.*;
 
+
+@Listeners({IInvokedMethodListenerClass.class, ITestResultListenerClass.class})
 public class TC01_LoginTest {
     private final String USERNAME = DataUtils.getJsonData("validLogin", "username");
     private final String PASSWORD = DataUtils.getJsonData("validLogin", "Password");
@@ -21,7 +25,9 @@ public class TC01_LoginTest {
     @BeforeMethod
     public void setup() throws IOException {
         setupDriver(DataUtils.getPropertyValue("environment", "Browser"));
+        LogsUtils.info("Browser was opened");
         getDriver().get(DataUtils.getPropertyValue("environment", "BASE_URL"));
+        LogsUtils.info(" browser is redirected to the url");
         getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
     }
@@ -36,7 +42,7 @@ public class TC01_LoginTest {
 
 
     @AfterMethod
-    public void quit() {
-
+    public void quit() throws IOException {
+        quitDriver();
     }
 }
