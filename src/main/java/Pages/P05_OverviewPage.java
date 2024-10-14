746f352f9bcfb586526cf062f296e004c9018aa7
Your Name <your.email@example.com>
2024-10-14 13:35:37 +0300

    7th TC part 1 finished

--- src/main/java/Pages/P05_OverviewPage.java
+++ src/main/java/Pages/P05_OverviewPage.java
@@ -1,11 +1,45 @@
 package Pages;
 
+import Utilities.Utility;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class P05_OverviewPage {
     private final WebDriver driver;
 
+    private final By subTotal = By.className("summary_subtotal_label");
+    private final By Tax = By.className("summary_tax_label");
+    private final By total = By.className("summary_total_label");
+    private final By finishButton = By.cssSelector("a[href=\"./checkout-complete.html\"]");
+
     public P05_OverviewPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    public Float getSubtotal() {
+        return Float.parseFloat(Utility.getText(driver, subTotal).replace("$", ""));
+    }
+
+    public Float getTax() {
+        return Float.parseFloat(Utility.getText(driver, Tax).replace("$", ""));
+    }
+
+    public Float getTotal() {
+        return Float.parseFloat(Utility.getText(driver, total).replace("$", ""));
+    }
+
+    public String calculateTotalPrice() {
+        return String.valueOf(getSubtotal() + getTax());
+    }
+
+    public Boolean comparingPrices() {
+        return calculateTotalPrice().equals(String.valueOf(getTotal()));
+    }
+
+    public P06_FinishingOrder clickOnFinishButton() {
+        Utility.clickingOnElement(driver, finishButton);
+        return new P06_FinishingOrder(driver);
+    }
+
+
 }
---
+++ src/main/java/Pages/P06_FinishingOrder.java
@@ -0,0 +1,8 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class P06_FinishingOrder {
+    public P06_FinishingOrder(WebDriver driver) {
+    }
+}
